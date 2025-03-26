92cf117c4fde835b60d60880b60138945a95f0b4
daniladouluvme <syvs0990@mail.ru>
2025-03-26 22:13:11 +0700

    Installed some packages

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,10 +1,20 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.scss";
+import { App } from "./components/App/App.tsx";
+import { createTheme, CssBaseline, ThemeProvider } from "@mui/material";
 
-createRoot(document.getElementById('root')!).render(
+const darkTheme = createTheme({
+  palette: {
+    mode: "dark",
+  },
+});
+
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <ThemeProvider theme={darkTheme}>
+      <CssBaseline />
+      <App />
+    </ThemeProvider>
+  </StrictMode>
+);
--- server/index.ts
+++ server/index.ts
@@ -1,1 +1,24 @@
-console.log("Hello via Bun!");
+import express, { json, urlencoded } from "express";
+import cors from "cors";
+import cookieParser from "cookie-parser";
+
+const app = express();
+const port = 3000;
+
+app.use(
+  cors({
+    origin: "http://localhost:5173", // Разрешаем фронтенду доступ
+    credentials: true, // ❗ Обязательно для отправки куков
+  })
+);
+app.use(json());
+app.use(cookieParser());
+app.use(urlencoded({ extended: true }));
+
+app.get("/api", (req, res) => {
+  res.send("Hello client");
+});
+
+app.listen(port, () => {
+  console.log(`The server is running on http://localhost:${port}`);
+});
